c5e77cc70ae9e86ff9d30454651acb2b010161a7
Ihar Rubanau <irubanau@Ihars-MacBook-Air.local>
2025-07-14 02:30:36 +0200

    Remove all .DS_Store files from nested folders

--- UIComponents/Tests/UIComponentsTests/UIComponentsTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import UIComponents
-
-final class UIComponentsTests: XCTestCase {
-    func testExample() throws {
-        // This is a placeholder test
-        XCTAssertTrue(true)
-    }
-} 
